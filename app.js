0401c4e8811b77b6608907aef47d426600d98d49
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-06-20 20:41:41 +0530

    feat: loader component with animation

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 const quoteDisplay = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const quoteFetchButton = document.querySelector('.quote-fetch-button');
+const loader = document.querySelector('#loader');
 
 
 quoteFetchButton.addEventListener('click', () => fetchQuotes());
 
 const fetchQuotes = async() => {
-  const res = await fetch('https://type.fit/api/quotes');
-  const data = await res.json();
-  pickRandomQuote(data);
+    loader.classList.add('show');
+    const res = await fetch('https://type.fit/api/quotes');
+    const data = await res.json();
+    pickRandomQuote(data);
+    loader.classList.remove('show');
 }
 
 const pickRandomQuote = (quotes) => {
-  const randomIndex = Math.floor(Math.random()*quotes.length);
-  
-  const quote = quotes[randomIndex];
-  renderQuote(quote);
+    const randomIndex = Math.floor(Math.random()*quotes.length);
+    const quote = quotes[randomIndex];
+    renderQuote(quote);
 }
 
 const renderQuote = (quote) => {
-  quoteDisplay.innerHTML = quote.text;
-  
-  (quote.author!==null) ? 
-  author.innerHTML = `- ${quote.author}` : 
-  author.innerHTML = "anonymous";
+    quoteDisplay.innerHTML = quote.text;
 
+    (quote.author!==null) ? 
+    author.innerHTML = `- ${quote.author}` : 
+    author.innerHTML = "anonymous";
 }
